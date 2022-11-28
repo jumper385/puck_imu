e07f494f416d8043fe558f289835b9195b9125db
Henry Chen <henrychen385@gmail.com>
2022-11-28 21:46:28 +0800

    removed build folder

--- nrf52dk_build/image_preload.cmake
+++
@@ -1,9 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD_ROOT "c:/Users/henry/Documents/embedded-projects/nrf52_adc;c:/Users/henry/Documents/embedded-projects/mpu6050;c:/Users/henry/Documents/embedded-projects/puck-imu;C:/ncs/v2.0.2/nrf;C:/ncs/v2.0.2/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "c:/Users/henry/Documents/embedded-projects/puck_imu/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONFIG_DEBUG_OPTIMIZATIONS "y" CACHE INTERNAL "NCS child image controlled")
-set(CONFIG_DEBUG_THREAD_INFO "y" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "c:/Users/henry/Documents/embedded-projects/puck_imu/nrf52dk_nrf52832.overlay" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "c:/Users/henry/Documents/embedded-projects/puck_imu/nrf52dk_nrf52832.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "c:\ncs\toolchains\v2.0.2\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
